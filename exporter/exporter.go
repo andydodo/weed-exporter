717ea8a560c2b6c559314cee32258826415c97e5
root <root@infra03.add.bjyt.qihoo.net>
2019-03-06 22:39:49 +0800

    modify exporter.go

--- exporter/exporter.go
+++ exporter/exporter.go
@@ -1,9 +1,13 @@
 package exporter
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os/exec"
+	"strings"
 	"sync"
+	"unicode"
 
 	"github.com/andy/weed-exporter/exporter/collectors"
 	"github.com/prometheus/client_golang/prometheus"
@@ -18,23 +22,60 @@ var _ prometheus.Collector = &WeedExporter{}
 
 func NewWeedExporter(server, path string) *WeedExporter {
 	var exporter *WeedExporter
-	var pathes []string
-	//Todo: 22 disks use this config
-	//parts := []string{":8001", ":8002", ":8003", ":8004", ":8005", ":8006", ":8007", ":8008", ":8009", ":8010",
-	//	":8011", ":8012", ":8013", ":8014", ":8015", ":8016", ":8017", ":8018", ":8019", ":8020", ":8021", ":8022"}
-	//Todo: 12 disks use this config
-	parts := []string{":8001", ":8002", ":8003", ":8004", ":8005", ":8006", ":8007", ":8008", ":8009", ":8010", ":8011", ":8012"}
-	//Todo: 11 disks use this config
-	//parts := []string{":8001", ":8002", ":8003", ":8004", ":8005", ":8006", ":8007", ":8008", ":8009", ":8010", ":8011"}
-	//Todo: 1 disks use this config
-	//parts := []string{":8001"}
-	//Todo: 5 disks use this config
-	//parts := []string{":8001", ":8002", ":8003", ":8004", ":8005"}
-
-	for _, part := range parts {
-		ip := path
-		ip += part
-		pathes = append(pathes, ip)
+	pathes := make([]string, 10)
+	var num string
+	cmd := `lsblk | grep data | awk -F\/ '{print $NF}' | wc -l`
+	out, err := exec.Command("bash", "-c", cmd).Output()
+	if err != nil {
+		log.Println(err)
+		return nil
+	} else {
+		num = strings.FieldsFunc(string(out), unicode.IsSpace)[0]
+	}
+	switch num {
+	case "22":
+		//Todo: 22 disks use this config
+		fmt.Println("ssssss")
+		parts := []string{":8001", ":8002", ":8003", ":8004", ":8005", ":8006", ":8007", ":8008", ":8009", ":8010", ":8011", ":8012", ":8013", ":8014", ":8015", ":8016", ":8017", ":8018", ":8019", ":8020", ":8021", ":8022"}
+		for _, part := range parts {
+			ip := path
+			ip += part
+			pathes = append(pathes, ip)
+		}
+	case "11":
+		//Todo: 11 disks use this config
+		parts := []string{":8001", ":8002", ":8003", ":8004", ":8005", ":8006", ":8007", ":8008", ":8009", ":8010", ":8011"}
+		for _, part := range parts {
+			ip := path
+			ip += part
+			pathes = append(pathes, ip)
+		}
+	case "1":
+		//Todo: 1 disks use this config
+		parts := []string{":8001"}
+		for _, part := range parts {
+			ip := path
+			ip += part
+			pathes = append(pathes, ip)
+		}
+	case "5":
+		//Todo: 5 disks use this config
+		parts := []string{":8001", ":8002", ":8003", ":8004", ":8005"}
+		for _, part := range parts {
+			ip := path
+			ip += part
+			pathes = append(pathes, ip)
+		}
+	case "12":
+		//Todo: 12 disks use this config
+		parts := []string{":8001", ":8002", ":8003", ":8004", ":8005", ":8006", ":8007", ":8008", ":8009", ":8010", ":8011", ":8012"}
+		for _, part := range parts {
+			ip := path
+			ip += part
+			pathes = append(pathes, ip)
+		}
+	default:
+		fmt.Println("hello")
 	}
 
 	switch server {
@@ -44,12 +85,16 @@ func NewWeedExporter(server, path string) *WeedExporter {
 				collectors.NewMasterCollector(path),
 			},
 		}
+		fmt.Println("a")
 	case "volume":
+		fmt.Println(pathes)
 		exporter = &WeedExporter{
 			collectors: []prometheus.Collector{
 				collectors.NewVolumeCollector(pathes),
 			},
 		}
+		fmt.Println("b")
+		fmt.Println(pathes)
 	}
 	return exporter
 }
